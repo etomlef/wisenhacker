7d1501538752b79e60950025ac9b78c92ecdb279
etomlef <etomlef@gmail.com>
2013-12-18 16:51:44 -0500

    Removed 'description' from Books model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205022934) do
+ActiveRecord::Schema.define(version: 20131218214937) do
 
   create_table "books", force: true do |t|
     t.string   "category"
     t.string   "subcat"
     t.string   "title"
     t.string   "link"
-    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
