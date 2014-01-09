1d3d1534f1204717ca828c087f8da4b53520f4d3
etomlef <etomlef@gmail.com>
2014-01-08 20:28:53 -0500

    fixed email link to direct to profile

--- db/migrate/20140108222839_create_book_lists.rb
+++ db/migrate/20140108222839_create_book_lists.rb
@@ -2,11 +2,6 @@ class CreateBookLists < ActiveRecord::Migration
   def change
     create_table :book_lists do |t|
       t.timestamps
-
-
-      user_id :integer 
-      book_id :integer 
-      complete :boolean
     end
   end
 end
